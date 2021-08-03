41f9c35e0e19d615918af8234565436485cd4bb3
AmirHossein_Ghotbeddini <43760972+amirGhotb@users.noreply.github.com>
2021-08-03 20:12:56 +0430

    Update Payment.js

--- src/Components/Payment.js
+++ src/Components/Payment.js
@@ -85,7 +85,7 @@ export default function Payment({userId,setLoading,group,type='radio',pageType='
         <form onSubmit={(e)=>{
             e.preventDefault()
             if (packageSelected.length >0){
-                let requireId = packages.filter(item=>item.number===0)[0].id
+                let requireId = packages.filter(item=>item.number===0)[0]?.id
                 if (packageSelected.includes(requireId)){
                     setPostPay(true)
                 }else{
@@ -95,43 +95,46 @@ export default function Payment({userId,setLoading,group,type='radio',pageType='
                 cogoToast.error('لطفا پکیج مورد نظر را انتخاب نمایید.')
             }
         }}>
-            <div className={'box bg-white p-lg-5 p-0 d-flex flex-column'} onChange={(e)=>{
-                let temp =packageSelected;
-                let value = parseInt(e.target.value)
-                if (e.target.type ==='radio'){
-                    temp =[value]
-                }else{
-                    if (temp.includes(value)){
-                        temp = temp.filter(item=>item!==value);
+            <div className={'box bg-white p-lg-5 p-0 d-flex flex-column'} >
+                <div onChange={(e)=>{
+                    let temp =packageSelected;
+                    let value = parseInt(e.target.value)
+                    if (e.target.type ==='radio'){
+                        temp =[value]
                     }else{
-                        temp.push(value)
+                        if (temp.includes(value)){
+                            temp = temp.filter(item=>item!==value);
+                        }else{
+                            temp.push(value)
+                        }
                     }
-                }
 
-                let s = 0
-                packages.forEach(item=>{
-                    if (temp.includes(item.id)){
-                        s += item.price
-                    }
-                })
-                setSumPrice(s)
-                setPackageSelected(temp)
-            }
-            }>
-                {['TAKHMIN_BEHDASHT','TAKHMIN_OLOOM'].includes(pageType) && packages.map((item,index)=>{
-                    return  <label key={index} htmlFor={`package-${item.id}`}>
-                        <input type={type} name={'package'} value={item.id} id={item.id} className={'mx-2'}/>
-                        تعداد {item.number} درخواست به مبلغ {item.price} تومان
-                    </label>
-                })}
-                {['ENTEKHAB_BEHDASHT','ENTEKHAB_OLOOM'].includes(pageType) && packages.map((item,index)=>{
-                    return <div className={'d-flex align-items-center text-left mt-4'}>
-                        <input type={type} name={'package'} checked={item.selected} value={item.id} id={item.id} className={'form-control col-2'} />
-                        <label className={'w-100 h5'} key={index} htmlFor={`${item.id}`}>
-                            {item.name} : {item.price} تومان
+                    let s = 0
+                    packages.forEach(item=>{
+                        if (temp.includes(item.id)){
+                            s += item.price
+                        }
+                    })
+                    setSumPrice(s)
+                    setPackageSelected(temp)
+                }
+                             }>
+                    {['TAKHMIN_BEHDASHT','TAKHMIN_OLOOM'].includes(pageType) && packages.map((item,index)=>{
+                        return  <label key={index} htmlFor={`package-${item.id}`}>
+                            <input type={type} name={'package'} value={item.id} id={item.id} className={'mx-2'}/>
+                            تعداد {item.number} درخواست به مبلغ {item.price} تومان
                         </label>
-                    </div>
-                })}
+                    })}
+                    {['ENTEKHAB_BEHDASHT','ENTEKHAB_OLOOM'].includes(pageType) && packages.map((item,index)=>{
+                        return <div className={'d-flex align-items-center text-left mt-4'}>
+                            <input type={type} name={'package'} checked={item.selected} value={item.id} id={item.id} className={'form-control col-2'} />
+                            <label className={'w-100 h5'} key={index} htmlFor={`${item.id}`}>
+                                {item.name} : {item.price} تومان
+                            </label>
+                        </div>
+                    })}
+                </div>
+
                 {['ENTEKHAB_BEHDASHT','ENTEKHAB_OLOOM'].includes(pageType) && <div className={'d-flex badge badge-primary justify-content-center mt-4'}>
                     <p className={' h6 m-0'}>قیمت : {sumPrice === 0 ? sumPrice : sumPrice - offValue} تومان </p>
                 </div>}
